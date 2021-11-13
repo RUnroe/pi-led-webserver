027a148b9c7a2dbf08d4ee4e560e8639c4f0e7c9
RUnroe <ryanunroe@gmail.com>
2021-11-12 21:16:31 -0700

    Remove logs

--- routes/routes.js
+++ routes/routes.js
@@ -44,7 +44,7 @@ const updateLights = (req, res) => {
       const options = [`-l ${brightness}`, `-r ${rgb.r}`, `-g ${rgb.g}`, `-b ${rgb.b}`, `-d ${delay}`].filter((argument) => argument.search('undefined') === -1);
   
       // Combine arguments and spawns child process
-      console.log(allRoutines, allRoutines[normalizeName(name)].path);
+    //   console.log(allRoutines, allRoutines[normalizeName(name)].path);
       const args = [allRoutines[normalizeName(name)].path, ...options];
       req.app.set('display', spawn('python', args));
   
@@ -54,7 +54,7 @@ const updateLights = (req, res) => {
 }
 
 const frontend = (req, res) => {
-    res.send("Hello RBGTQ world");
+    res.send("Hello RGBTQ world");
 }
 
 const routes = [
